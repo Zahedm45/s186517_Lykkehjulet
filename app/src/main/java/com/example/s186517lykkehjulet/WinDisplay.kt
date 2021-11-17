d7b9432d8cc06b826c811901b4c20b2938d355e7
Zahed <zahedm45@gmail.com>
2021-11-17 23:37:43 +0100

    fragment for winDisplay works

--- app/src/main/java/com/example/s186517lykkehjulet/WinDisplay.kt
+++ app/src/main/java/com/example/s186517lykkehjulet/WinDisplay.kt
@@ -4,7 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
+import kotlin.system.exitProcess
 
 class WinDisplay : Fragment() {
 
@@ -13,6 +16,16 @@ class WinDisplay : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_win_display, container, false)
+        val view = inflater.inflate(R.layout.fragment_win_display, container, false)
+        view.findViewById<Button>(R.id.playButton).setOnClickListener {
+            Navigation.findNavController(view).navigate(R.id.wordsReFragment)
+        }
+
+        view.findViewById<Button>(R.id.stopButton).setOnClickListener {
+
+            activity?.finish()
+            exitProcess(0)
+        }
+        return view
     }
 }
